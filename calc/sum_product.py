07e189d5f5851a56852161b1044bc8f82c0adb4e
abcdragon <dragonabc123@naver.com>
2020-06-26 00:05:49 +0900

    Issue #4

---
+++ calc/sum_product.py
@@ -0,0 +1,19 @@
+from cgi import parse_qs
+from template import html
+
+def application(environ, start_response):
+	d = parse_qs(environ['QUERY_STRING'])
+	a = d.get('a', [''])[0]
+	b = d.get('b', [''])[0]
+	response_body = html.split('\n')
+	if '' not in [a, b]:
+		a, b = int(a), int(b)
+		response_body[10] += str(a+b)
+		response_body[11] += str(a*b)
+	response_body = '\n'.join(response_body)
+	start_response('200 OK', [
+		('Content-Type', 'text/html'),
+		('Content-Length', str(len(response_body)))
+	])
+	return [response_body]
+
---
+++ calc/template.py
@@ -0,0 +1,15 @@
+html = b"""
+<html>
+	<body>
+		<form action="">
+			a : <input type="number" name="a">
+			<br><br>
+			b : <input type="number" name="b">
+			<br><br>
+			<input type="submit"><br>
+		</form>
+		a + b : 
+		<br><br>a * b : 
+	</body>
+</html>
+"""
